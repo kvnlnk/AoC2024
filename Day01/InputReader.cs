1f1604945c3aaa8e0f485cbae5f10299da6acf37
Kevin Lingk <klingk.msg@gmail.com>
2024-12-01 22:03:29 +0100

    feat: add InputReader to read text files

---
+++ Day01/InputReader.cs
@@ -0,0 +1,24 @@
+﻿namespace AoC2024.Day01;
+
+public class InputReader
+{
+    private readonly string day;
+
+    public InputReader(string day)
+    {
+        this.day = day;
+    }
+
+    public List<string> ReadFile()
+    {
+        var stringList = new List<string>();
+
+        using var streamReader = new StreamReader($@"Day{day}/Input.txt");
+        while (streamReader.ReadLine() is { } line)
+        {
+            stringList.Add(line);
+        }
+
+        return stringList;
+    }
+}
