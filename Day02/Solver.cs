90ea2a58abe87c1454f12729c4fab578b3850820
Kevin Lingk <klingk.msg@gmail.com>
2024-12-04 22:11:20 +0100

    Revert "style: change output formating"
    This reverts commit 84423f25349b384bd83285294d2cbf0354b79f19.

--- Day02/Solver.cs
+++ Day02/Solver.cs
@@ -30,133 +30,52 @@ public class Solver : ISolver
     public string GetPartOneSolution()
     {
         var safeCounter = 0;
+        var valid = true;
 
         foreach (var list in _listOfLists)
         {
-            if (IsSafe(list))
-            {
-                safeCounter++;
-            }
-        }
-
-        return safeCounter.ToString();
-    }
-
-    public string GetPartTwoSolution()
-    {
-        var safeCounter = 0;
-
-        foreach (var list in _listOfLists)
-        {
-            var valid = true;
-            var copyOfList = new List<int>(list);
             // Check first two numbers to determine if they are increasing or decreasing
             if (list[0] < list[1]) // increase
             {
                 for (var i = 0; i < list.Count - 1; i++)
                 {
-                    if (list[i] <= list[i + 1] && list[i + 1] - list[i] is >= 1 and <= 3) continue; // valid case
-
-                    if (TryToMakeSafe(copyOfList, list))
+                    if (list[i] > list[i + 1] || (list[i + 1] - list[i] is < 1 or > 3)) // invalid case
                     {
-                        safeCounter++;
+                        valid = false;
+                        break;
                     }
+                }
 
-                    valid = false;
-                    break;
+                if (valid)
+                {
+                    safeCounter++;
                 }
             }
             else if (list[0] > list[1]) // decrease
             {
                 for (var i = 0; i < list.Count - 1; i++)
                 {
-                    if (list[i] >= list[i + 1] && (list[i] - list[i + 1] is >= 1 and <= 3)) continue; // valid case
-
-                    if (TryToMakeSafe(copyOfList, list))
+                    if (list[i] < list[i + 1] || (list[i] - list[i + 1] is < 1 or > 3)) // invalid case
                     {
-                        safeCounter++;
+                        valid = false;
+                        break;
                     }
-
-                    valid = false;
-                    break;
                 }
-            }
-            else // equal numbers at the beginning
-            {
-                if (TryToMakeSafe(copyOfList, list))
+
+                if (valid)
                 {
                     safeCounter++;
                 }
-
-                valid = false;
             }
 
-            // Valid if no list needed to be fixed
-            if (valid)
-            {
-                safeCounter++;
-            }
+            valid = true;
         }
-
+        
         return safeCounter.ToString();
     }
 
-
-    private static bool IsSafe(List<int> list)
+    public string GetPartTwoSolution()
     {
-        var valid = true;
-        // Check first two numbers to determine if they are increasing or decreasing
-        if (list[0] < list[1]) // increase
-        {
-            for (var i = 0; i < list.Count - 1; i++)
-            {
-                if (list[i] > list[i + 1] || (list[i + 1] - list[i] is < 1 or > 3)) // invalid case
-                {
-                    valid = false;
-                    break;
-                }
-            }
-
-            if (valid)
-            {
-                return true;
-            }
-        }
-        else if (list[0] > list[1]) // decrease
-        {
-            for (var i = 0; i < list.Count - 1; i++)
-            {
-                if (list[i] < list[i + 1] || (list[i] - list[i + 1] is < 1 or > 3)) // invalid case
-                {
-                    valid = false;
-                    break;
-                }
-            }
-
-            if (valid)
-            {
-                return true;
-            }
-        }
-
-        return false;
-    }
-
-
-    private static bool TryToMakeSafe(List<int> copyOfList, List<int> list)
-    {
-        for (var j = 0; j < list.Count; j++)
-        {
-            copyOfList.RemoveAt(j);
-            if (IsSafe(copyOfList))
-            {
-                return true;
-            }
-
-            copyOfList.Clear();
-            copyOfList.AddRange(list);
-        }
-
-        return false;
+        throw new NotImplementedException();
     }
 }
