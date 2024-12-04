677eba23a30a9f84983a760afa1f96ea45c80e14
Kevin Lingk <klingk.msg@gmail.com>
2024-12-04 22:08:37 +0100

    refac: remove redundant code

--- Day02/Solver.cs
+++ Day02/Solver.cs
@@ -30,52 +30,133 @@ public class Solver : ISolver
     public string GetPartOneSolution()
     {
         var safeCounter = 0;
-        var valid = true;
 
         foreach (var list in _listOfLists)
         {
+            if (IsSafe(list))
+            {
+                safeCounter++;
+            }
+        }
+
+        return safeCounter.ToString();
+    }
+
+    public string GetPartTwoSolution()
+    {
+        var safeCounter = 0;
+
+        foreach (var list in _listOfLists)
+        {
+            var valid = true;
+            var copyOfList = new List<int>(list);
             // Check first two numbers to determine if they are increasing or decreasing
             if (list[0] < list[1]) // increase
             {
                 for (var i = 0; i < list.Count - 1; i++)
                 {
-                    if (list[i] > list[i + 1] || (list[i + 1] - list[i] is < 1 or > 3)) // invalid case
-                    {
-                        valid = false;
-                        break;
-                    }
-                }
+                    if (list[i] <= list[i + 1] && list[i + 1] - list[i] is >= 1 and <= 3) continue; // valid case
 
-                if (valid)
-                {
-                    safeCounter++;
+                    if (TryToMakeSafe(copyOfList, list))
+                    {
+                        safeCounter++;
+                    }
+
+                    valid = false;
+                    break;
                 }
             }
             else if (list[0] > list[1]) // decrease
             {
                 for (var i = 0; i < list.Count - 1; i++)
                 {
-                    if (list[i] < list[i + 1] || (list[i] - list[i + 1] is < 1 or > 3)) // invalid case
-                    {
-                        valid = false;
-                        break;
-                    }
-                }
+                    if (list[i] >= list[i + 1] && (list[i] - list[i + 1] is >= 1 and <= 3)) continue; // valid case
 
-                if (valid)
+                    if (TryToMakeSafe(copyOfList, list))
+                    {
+                        safeCounter++;
+                    }
+
+                    valid = false;
+                    break;
+                }
+            }
+            else // equal numbers at the beginning
+            {
+                if (TryToMakeSafe(copyOfList, list))
                 {
                     safeCounter++;
                 }
+
+                valid = false;
             }
 
-            valid = true;
+            // Valid if no list needed to be fixed
+            if (valid)
+            {
+                safeCounter++;
+            }
         }
-        
+
         return safeCounter.ToString();
     }
 
-    public string GetPartTwoSolution()
+
+    private static bool IsSafe(List<int> list)
     {
-        throw new NotImplementedException();
+        var valid = true;
+        // Check first two numbers to determine if they are increasing or decreasing
+        if (list[0] < list[1]) // increase
+        {
+            for (var i = 0; i < list.Count - 1; i++)
+            {
+                if (list[i] > list[i + 1] || (list[i + 1] - list[i] is < 1 or > 3)) // invalid case
+                {
+                    valid = false;
+                    break;
+                }
+            }
+
+            if (valid)
+            {
+                return true;
+            }
+        }
+        else if (list[0] > list[1]) // decrease
+        {
+            for (var i = 0; i < list.Count - 1; i++)
+            {
+                if (list[i] < list[i + 1] || (list[i] - list[i + 1] is < 1 or > 3)) // invalid case
+                {
+                    valid = false;
+                    break;
+                }
+            }
+
+            if (valid)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+
+    private static bool TryToMakeSafe(List<int> copyOfList, List<int> list)
+    {
+        for (var j = 0; j < list.Count; j++)
+        {
+            copyOfList.RemoveAt(j);
+            if (IsSafe(copyOfList))
+            {
+                return true;
+            }
+
+            copyOfList.Clear();
+            copyOfList.AddRange(list);
+        }
+
+        return false;
     }
 }
