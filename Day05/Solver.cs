f067246e95a50d3ac254fa1e8e2ee9ea5fead201
klingk.msg@gmail.com <klingk.msg@gmail.com>
2024-12-06 10:42:43 +0100

    feat: add solution for part 2

--- Day05/Solver.cs
+++ Day05/Solver.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using AoC2024.Common;
+﻿using AoC2024.Common;
 
 namespace AoC2024.Day05;
 
@@ -10,6 +7,7 @@ public class Solver : ISolver
     private readonly List<string> _list;
     private readonly List<List<int>> _ruleList = [];
     private readonly List<List<int>> _listOfPages = [];
+    private readonly List<List<int>> _listOfWrongPages = [];
 
     public Solver(List<string> list)
     {
@@ -43,23 +41,21 @@ public class Solver : ISolver
     {
         var sum = 0;
         
-        // try with first list of pages
-        for (var p = 0; p < _listOfPages.Count; p++)
+        foreach (var page in _listOfPages)
         {
-
             var valid = true;
-            for (var i = 0; i < _listOfPages[p].Count; i++)
+            for (var i = 0; i < page.Count; i++)
             {
                 
                 // Get all rules for the current page
-                var pageRules = _ruleList.Where(rule => rule[0] == _listOfPages[p][i]).ToList();
+                var pageRules = _ruleList.Where(rule => rule[0] == page[i]).ToList();
 
 
-                for (var j = 0; j < _listOfPages[p].Count; j++)
+                for (var j = 0; j < page.Count; j++)
                 {
                     foreach (var rule in pageRules)
                     {
-                        if (_listOfPages[p][j] == rule[1])
+                        if (page[j] == rule[1])
                         {
 
                             // Index of page > Index of rule
@@ -73,16 +69,75 @@ public class Solver : ISolver
             }
             if (valid)
             {
-                sum += _listOfPages[p][_listOfPages[p].Count / 2];
+                sum += page[page.Count / 2];
             }
-
+            else
+            {
+                _listOfWrongPages.Add(page);
+            }
         }
-
+        return sum.ToString();
+    }
+    
+    public string GetPartTwoSolution()
+    {
+        var sum = 0;
+        
+        foreach (var pages in _listOfWrongPages)
+        {
+            for (var i = 0; i < pages.Count; i++)
+            {
+                
+                // Get all rules for the current page
+                var pageRules = _ruleList.Where(rule => rule[0] == pages[i]).ToList();
+                
+                // Iterate through all pages of a list to find possible rules in the list
+                for (var j = 0; j < pages.Count; j++)
+                {
+                    foreach (var rule in pageRules)
+                    {
+                        // Find possible rule in the list
+                        if (pages[j] == rule[1])
+                        {
+                            // If the index of the page (i) value is greater than the
+                            // index of the rule position (j), the rule is broken
+                            if (i > j)
+                            {
+                                SortPagesIfWrong(pages);
+                            }
+                        }
+                    }
+                }
+            }
+            sum += pages[pages.Count / 2];
+        }
         return sum.ToString();
     }
 
-    public string GetPartTwoSolution()
+    private void SortPagesIfWrong(List<int> list)
     {
-        throw new NotImplementedException();
+        for (var i = 0; i < list.Count; i++)
+        {
+            // Get all rules for the current page
+            var pageRules = _ruleList.Where(rule => rule[0] == list[i]).ToList();
+            
+            // Iterate through list and change indices if rule is broken
+            for (var j = 0; j < list.Count; j++)
+            {
+                foreach (var rule in pageRules)
+                {
+                    if (list[j] == rule[1])
+                    {
+                        // If the index of the page value is greater than the index of the rule, the rule is "broken"
+                        if (i > j)
+                        {
+                            var page = list[j];
+                            list.RemoveAt(j);
+                            list.Insert(i, page);
+                        }
+                    }
+                }
+            }
+        }
     }
 }
