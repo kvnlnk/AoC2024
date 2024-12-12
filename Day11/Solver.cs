6de1e089b9d1187c7992b511fe19ad13bece151a
Kevin Lingk <klingk.msg@gmail.com>
2024-12-12 22:42:06 +0100

    feat: add solution for part 2 day 11

--- Day11/Solver.cs
+++ Day11/Solver.cs
@@ -7,7 +7,8 @@ public class Solver : ISolver
 {
     private readonly List<string> _list;
     private readonly List<string> _stones = [];
-    
+    private readonly List<string> _stones2 = [];
+
     public Solver(List<string> list)
     {
         _list = list;
@@ -19,11 +20,12 @@ public class Solver : ISolver
         foreach (var stone in _list[0].Split(" "))
         {
             _stones.Add(stone);
+            _stones2.Add(stone);
         }
-            
+
         Console.WriteLine(string.Join(",", _stones));
     }
-    
+
     public string GetPartOneSolution()
     {
         for (var i = 0; i < 25; i++)
@@ -39,7 +41,7 @@ public class Solver : ISolver
                     var stone = _stones[j];
                     // Insert first half of the number into its old place
                     _stones[j] = _stones[j][..(_stones[j].Length / 2)];
-                    
+
                     // Get substring and remove leading 0 and add it to next to the old numbr
                     var substring = stone.Substring(stone.Length / 2, stone.Length / 2).TrimStart(['0']);
                     _stones.Insert(j + 1, substring == string.Empty ? "0" : substring);
@@ -58,6 +60,75 @@ public class Solver : ISolver
 
     public string GetPartTwoSolution()
     {
-        throw new NotImplementedException();
+        // Cache already processed values
+        var memoization = new Dictionary<string, List<string>>(); 
+
+        // Store stone and count of stone occurrences
+        var stones = new Dictionary<string, long>();
+        foreach (var stone in _stones2)
+        {
+            stones.TryAdd(stone, 0);
+            stones[stone]++;
+        }
+
+        for (var i = 0; i < 75; i++)
+        {
+            var newStones = new Dictionary<string, long>();
+
+            foreach (var (originalStone, count) in stones)
+            {
+                // Try to find values of already processed stone to skip operations
+                if (memoization.TryGetValue(originalStone, out var value))
+                {
+                    foreach (var stone in value)
+                    {
+                        newStones.TryAdd(stone, 0);
+                        newStones[stone] += count;
+                    }
+
+                    continue;
+                }
+
+                if (originalStone == "0")
+                {
+                    newStones.TryAdd("1", 0);
+                    newStones["1"] += count;
+                    memoization[originalStone] = ["1"];
+                }
+                else if (originalStone.Length % 2 == 0)
+                {
+                    var firstHalf = originalStone[..(originalStone.Length / 2)];
+                    var secondHalf = originalStone[(originalStone.Length / 2)..].TrimStart('0');
+                    
+                    if (secondHalf == string.Empty)
+                    {
+                        secondHalf = "0";
+                    }
+
+                    newStones.TryAdd(firstHalf, 0);
+                    newStones[firstHalf] += count;
+
+                    newStones.TryAdd(secondHalf, 0);
+                    newStones[secondHalf] += count;
+
+                    // Cache processed values for original stone
+                    memoization[originalStone] = [firstHalf, secondHalf];
+                }
+                else
+                {
+                    var multipliedValue = (Convert.ToInt64(originalStone) * 2024).ToString();
+
+                    newStones.TryAdd(multipliedValue, 0);
+                    newStones[multipliedValue] += count;
+
+                    memoization[originalStone] = [multipliedValue];
+                }
+            }
+
+            stones = newStones;
+        }
+
+        return stones.Values.Sum().ToString();
     }
+
 }
